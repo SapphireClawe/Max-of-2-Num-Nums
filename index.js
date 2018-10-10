572ff1d86a4a22768602b45e00896aaf6a9d87f1
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-10-10 13:41:44 -0600

    Check

---
+++ index.js
@@ -0,0 +1,13 @@
+function max (number1, number2) {
+  if (number1 > number2) return number1;
+  else if (number1 < number2) return number2;
+  else square = true;
+  console.log("The function didn't run properly");
+
+}
+
+let square = false;
+
+let number = max (, );
+if (square == true) console.log("The numbers are the same.");
+elseconsole.log("The larger number is", number);
